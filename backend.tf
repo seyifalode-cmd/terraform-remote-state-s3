a3025329e7c368dc07f9ffced3e5bbf94c603a29
SMannion YorkU <smannion@yorku.ca>
2023-11-08 22:46:55 -0500

    first commit

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3"{
+    key = "myStateFile.tfstate" # you can provide a different state file name if you wish
+    # enter your s3 bucket name here -> bucket = ""
+    region = "us-east-1"
+    # enter your credentials here -> access_key = ""
+    # enter your credentials here -> secret_key = ""
+  }
+}
